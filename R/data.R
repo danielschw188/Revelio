48e3ce9094a029cc44bcec21adc422c6870a331e
danielschw188 <daniel.schwabe@gmx.de>
2020-10-05 17:03:37 +0200

    included sample data into package

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A single-cell gene expression data matrix.
+#'
+#' A dataset that contains a gene-by-cell matrix with UMI counts as entries.
+#'
+#' @format A data frame with 22717 rows (genes) and 1477 variables (cells).
+"revelioTestData_rawDataMatrix"
+#'
+#'
+#' A table of marker gene lists.
+#'
+#' A dataset that contains multiple lists of marker genes corresponding to cell cycle phases or cell cycle phase transitions.
+#'
+#' @format A data frame with 216 rows (length of the longest marker gene list) and 5 variables (cell cycle phases and transitions: G1.S, S, G2, G2.M, M.G1).
+"revelioTestData_cyclicGenes"
